4a064bdce391f2d092177f35ac5e19c30f9558d7
shawnsmothers <59694659+shawnsmothers@users.noreply.github.com>
2021-01-10 08:25:46 -0600

    workshopweek2

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
+const mongoose = require('mongoose')
+
+const url = 'mongodb://localhost:27017/nucampsite';
+
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
+
 var app = express();
 
 // view engine setup
@@ -20,7 +39,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+
 app.use('/users', usersRouter);
+app.use('/campsites', campsiteRouter);
+app.use('/promotions',promotionRouter);
+app.use('/partners',partnerRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +62,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  console.log('here in index')
+  res.send('index route hit')
 });
 
 module.exports = router;
