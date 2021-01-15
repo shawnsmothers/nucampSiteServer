610f944618c47d9a09e0cc071eaad6bf87e814a3
shawnsmothers <59694659+shawnsmothers@users.noreply.github.com>
2021-01-14 22:14:57 -0600

    Passport:2

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
-const passport = require("passport");
-const authenticate = require('./authenticate');
 
 const mongoose = require("mongoose");
 
@@ -41,7 +39,8 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser('12345-67890-09876-54321'));//the string key can be anything
+
+//app.use(cookieParser("12345-67890-09876-54321"));
 
 app.use(
   session({
@@ -54,28 +53,26 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use("/users", usersRouter);
-app.use("/campsites", campsiteRouter);
-//custom middleware function: add authentication
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
   console.log(req.user);
 
   if (!req.user) {
-    const err = new Error("You are not authenticated!");
-    err.status = 401;
-    return next(err);
+      const err = new Error('You are not authenticated!');                    
+      err.status = 401;
+      return next(err);
   } else {
-    return next();
+      return next();
   }
 }
 
 app.use(auth);
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-
 app.use("/users", usersRouter);
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose')
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
     admin: {
@@ -10,3 +10,5 @@ const userSchema = new Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
